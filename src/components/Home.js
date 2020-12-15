03afe76ba56fc40f7ed52dbdf28bfd5d39d65627
Manny Garcia <mannyalgarcia@gmail.com>
2020-12-15 16:44:33 -0500

    Moved styles from main.css to styled components in links.js and home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,86 @@
 import React from 'react'
-import {
-  MdTouchApp,
-} from "react-icons/md"
+import { MdTouchApp } from "react-icons/md"
+import styled from 'styled-components'
 
 const Home = () => {
   const background = document.querySelector('body')
   return (
-    <React.Fragment>
-      {background.setAttribute('style', 'background: #000')}
-      <div className="intro-container" onClick={event =>  window.location.href='/links'}>
-        <div className="intro-greeting-lg">
-          Nice to Meet You.
+    <>
+      <Wrapper>
+        {background.setAttribute('style', 'background: #000')}
+        <div className="intro-container" onClick={event =>  window.location.href='/links'}>
+          <div className="intro-greeting-lg">
+            Nice to Meet You.
+          </div>
+          
+          <div className="intro-greeting-sm">
+          Hello.
+          </div>
+          
+          <div className="intro-name">
+            I'm Manny
+          </div>
+    
+          <div className="intro-tap">
+            <MdTouchApp className="pointer-icon"></MdTouchApp>
+            {' '}
+            tap to begin
+          </div>
         </div>
-        
-        <div className="intro-greeting-sm">
-        Hello.
-        </div>
-        
-        <div className="intro-name">
-          I'm Manny
-        </div>
-        
-        {/* <div className="intro-name">
-          software engineer
-        </div> */}
-        <div className="intro-tap">
-          <MdTouchApp className="pointer-icon"></MdTouchApp>
-          {' '}
-          tap to begin
-        </div>
-      </div>
-    </React.Fragment>
+      </Wrapper>
+    </>
     )
 }
+
+const Wrapper = styled.section`
+  .intro-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+    margin-left: 4rem;
+    cursor: pointer;
+  }
+  
+  .intro-greeting-sm {
+    display: none
+  }
+  
+  .intro-greeting-lg {
+    font-size: 6rem;
+    font-weight: bold;
+  }
+  
+  .intro-name {
+    color: #e775fc;
+    font-size: 6rem;
+    font-weight: bold
+  }
+  
+  .intro-tap {
+    margin-top: 2rem;
+    text-align: center;
+  }
+  
+  @media only screen and (max-width: 700px) {
+      .intro-container {
+      margin-left: 3rem;
+    }
+    .intro-greeting-lg {
+      display: none;
+    }
+    .intro-greeting-sm {
+      display: inline;
+      font-size: 5rem;
+      font-weight: bold;
+    }
+    .intro-name {
+      color: #e775fc;
+      font-size: 5rem;
+      font-weight: bold;
+    }
+  }
+  
+`
 
 export default Home
--- src/pages/links.js
+++ src/pages/links.js
@@ -1,31 +1,59 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from 'styled-components'
+
 const Links = () => {
   const background = document.querySelector('body')
   return (
-    <React.Fragment>
-    {background.setAttribute('style', 'background: #000')}
-    <ul className="page-links">
-      <li className="link-about">
-        <Link to="/about" >
-          About
-        </Link>
-      </li>
-      <li className="link-work">
-        <Link to="/work" >
-          Work
-        </Link>
-      </li>
-      <li className="link-contact">
-        <Link to="/contact" >
-          Contact
-        </Link>
-      </li>
-      <li className="intro-tap">
-      </li>
-    </ul>
-    </React.Fragment>
+    <>
+      <Wrapper>
+        {background.setAttribute('style', 'background: #000')}
+        <ul className="page-links">
+          <li className="link-about">
+            <Link to="/about" >
+              About
+            </Link>
+          </li>
+          <li className="link-work">
+            <Link to="/work" >
+              Work
+            </Link>
+          </li>
+          <li className="link-contact">
+            <Link to="/contact" >
+              Contact
+            </Link>
+          </li>
+          <li className="intro-tap">
+          </li>
+        </ul>
+      </Wrapper>
+    </>
   )
 }
+
+const Wrapper = styled.section`
+  .page-links {
+    margin-right: 4rem;
+  }
+  
+  .link-about,
+  .link-work,
+  .link-contact {
+    font-size: 6rem;
+  }
+  
+  @media only screen and (max-width: 700px) {
+    .page-links {
+      margin-right: 2rem;
+    }
+    
+    .link-about,
+    .link-work,
+    .link-contact {
+      font-size: 4.9rem;
+    }
+  }
+`
 
 export default Links
