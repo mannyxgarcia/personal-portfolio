73ed9890f3b2b6dd7fdb4c083a5f262a71fa67a8
Manny Garcia <mannyalgarcia@gmail.com>
2020-12-15 17:07:38 -0500

    Moved styles from main.css to styled components in about.js and socialLinks.js

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from 'styled-components'
 import {
   FaLinkedin,
   FaGithubSquare,
@@ -6,23 +7,58 @@ import {
 } from "react-icons/fa"
 const SocialLinks = () => {
   return (
-    <ul className="social-icon-container">
-      <li>
-        <a href="https://www.linkedin.com/in/mannyxgarcia/">
-          <FaLinkedin className="social-icon linkedin-icon"></FaLinkedin>
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/mannyxgarcia">
-          <FaGithubSquare className="social-icon github-icon"></FaGithubSquare>
-        </a>
-      </li>
-      <li>
-        <a href="https://twitter.com">
-          <FaTwitterSquare className="social-icon twitter-icon"></FaTwitterSquare>
-        </a>
-      </li>
-    </ul>
+    <>
+      <Wrapper>
+        <ul className="social-icon-container">
+          <li>
+            <a href="https://www.linkedin.com/in/mannyxgarcia/">
+              <FaLinkedin className="social-icon linkedin-icon"></FaLinkedin>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/mannyxgarcia">
+              <FaGithubSquare className="social-icon github-icon"></FaGithubSquare>
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com">
+              <FaTwitterSquare className="social-icon twitter-icon"></FaTwitterSquare>
+            </a>
+          </li>
+        </ul>
+      </Wrapper>
+    </>
   )
 }
+
+const Wrapper = styled.section`
+  .social-icon-container {
+    display: flex;
+    margin-bottom: 0.5rem;
+  }
+  
+  .social-icon {
+    font-size: 2rem;
+    transition: all 0.3s linear;
+    margin-right: 0.5rem;
+  }
+
+  .social-icon:hover {
+    color: #fff;
+    transform: translateY(-5px);
+  }
+  
+  .linkedin-icon {
+    color: #3b5998;
+  }
+  
+  .twitter-icon {
+    color: #00acee;
+  }
+  
+  .github-icon {
+    color: #494949;
+  }
+`
+
 export default SocialLinks
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,34 +3,79 @@ import Navbar from '../components/Navbar'
 import SocialLinks from '../constants/socialLinks'
 import {BsDownload} from 'react-icons/bs'
 import resume from '../constants/mgarcia-resume-12-20.pdf'
+import styled from 'styled-components'
 
 const About = () => {
   const background = document.querySelector('body')
   return (
-    <React.Fragment>
-      {background.setAttribute('style', 'background: #ea88fc')}
-      <Navbar></Navbar>
-      <div className="main-container">
-        <div className="header">
-          <h1>I am a full stack developer.</h1>
+    <>
+      <Wrapper>
+        {background.setAttribute('style', 'background: #ea88fc')}
+        <Navbar/>
+        <div className="main-container">
+          <div className="header">
+            <h1>I am a full stack developer.</h1>
+          </div>
+          <div className="secondary-container">
+            <div className="social-links">
+              <SocialLinks />
+            </div>
+            <div className="content-container">
+              I am a full stack web developer with a passion for front-end web developement. I have 4 years of work experience in design for theatrical entertainment. I transitioned to software engineering to pursue a previous passion I had developed in my early high school years. I enjoy building out the visual components of a design and layout with careful consideration for the overall user experience. I blend my knowledge in design and technical skills to collaborate and create products with thoughtful and creative solutions. 
+            </div>
+            <div className="resume-link download-icon">
+            <a href={resume} download="mgarcia-resume.pdf"> <BsDownload/> download resume</a>
+            </div>
+          </div>
         </div>
-        <div className="about-container">
-          <div className="social-links">
-            <SocialLinks />
-          </div>
-          <div className="content-container">
-            I am a full stack web developer with a passion for front-end web developement. I have 4 years of work experience in design for theatrical entertainment. I transitioned to software engineering to pursue a previous passion I had developed in my early high school years. I enjoy building out the visual components of a design and layout with careful consideration for the overall user experience. I blend my knowledge in design and technical skills to collaborate and create products with thoughtful and creative solutions. 
-            
-          </div>
-          <div className="resume-link download-icon">
-          <a href={resume} download="mgarcia-resume.pdf"> <BsDownload/> download resume</a>
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
+      </Wrapper>
+    </>
   )
 }
 
-
+const Wrapper = styled.section`
+  .main-container {
+    margin: 1rem 4rem 1rem 10rem;
+  }
+  
+  .header {
+  height: 40vh;
+  }
+  
+  .secondary-container {
+    padding: 0 8rem 0 4rem;
+  }
+  
+  .content-container {
+  padding: 1.5rem;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  color: #fff;
+  }
+  
+  .resume-link {
+  margin-top: 0.5rem;
+  }
+  
+  .download-icon {
+  font-size: 1.1rem;
+  transition: all 0.3s linear;
+  padding-top: 0.5rem;
+  }
+  
+  .download-icon:hover {
+  transform: translateY(-5px);
+  }
+  
+  @media only screen and (max-width: 700px) {
+    .main-container {
+      margin: 3rem 1rem 1.5rem 1.5rem
+    }
+    
+    .secondary-container {
+      padding: 0
+    }
+  }
+`
 
 export default About
