81b5e8feceb5d4f15c23771c094c06cdbfff375c
Manny Garcia <mannyalgarcia@gmail.com>
2020-12-15 17:49:32 -0500

    Moved styles from main.css to links.js and Navbar.js styled component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import NavLinks from '../constants/links'
+import styled from 'styled-components'
 
 const Navbar = () => {
   return (
-  <nav className="navbar">
-        <Link to="/">
-        </Link>
-      <NavLinks  />
-  </nav>
+    <>
+      <Wrapper>
+        <nav className="navbar">
+          <Link to="/"></Link>
+          <NavLinks  />
+        </nav>
+      </Wrapper>
+    </>
   )
 }
+
+const Wrapper = styled.section`
+  .navbar {
+  background: #3b3b3b;
+  padding: 0.5rem;
+  }
+`
 
 export default Navbar
--- src/constants/links.js
+++ src/constants/links.js
@@ -1,31 +1,51 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from 'styled-components'
+
 const Links = () => {
   return (
-    <ul className="nav-links">
-      <li className="link">
-        <Link to="/" >
-        Home
-        </Link>
-      </li>
-      <li className="link">
-        <Link to="/about" >
-          About
-        </Link>
-      </li>
-      <li className="link">
-        <Link to="/work" >
-          Work
-        </Link>
-      </li>
-      <li className="link">
-        <Link to="/contact" >
-          Contact
-        </Link>
-      </li>
-
-    </ul>
+    <>
+      <Wrapper>
+        <ul className="nav-links">
+          <li className="link">
+            <Link to="/" >
+            Home
+            </Link>
+          </li>
+          <li className="link">
+            <Link to="/about" >
+              About
+            </Link>
+          </li>
+          <li className="link">
+            <Link to="/work" >
+              Work
+            </Link>
+          </li>
+          <li className="link">
+            <Link to="/contact" >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </Wrapper>
+    </>
   )
 }
+
+const Wrapper = styled.section`
+  .nav-links {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .link {
+    color: #d8d8d8;
+  }
+
+  .link:hover {
+    color: #fff;
+  }
+`
 
 export default Links
