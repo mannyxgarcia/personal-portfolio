ec40c031e3a54c525c6f6f0462285e73b9eb797e
Manny Garcia <mannyalgarcia@gmail.com>
2020-12-16 14:20:55 -0500

    Feature- Added fade-in transition to work-template.js

--- src/templates/work-template.js
+++ src/templates/work-template.js
@@ -19,7 +19,7 @@ const WorkTemplate = ({data}) => {
       <Navbar toggle={toggle}/>
         <Sidebar isOpen={isOpen} toggle={toggle} />
       <Wrapper>
-        <div className='container'>
+        <div className='container fade-in'>
           <div className="title">
             <h1>{title}</h1>
             <div className="subTitle">{subTitle}</div>
@@ -102,6 +102,19 @@ const Wrapper = styled.section`
     }
     .body{
       width: 80%
+    }
+  }
+  
+  .fade-in {
+    animation: fadeIn ease 2s;
+  }
+  
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
     }
   }
   
